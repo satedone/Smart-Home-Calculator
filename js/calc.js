fb2453ac52309a5b57b7a9dfd7d1950afc098515
satedone <nz@intop.dp.ua>
2023-07-14 18:39:40 +0300

    Choose an offer

--- js/calc.js
+++ js/calc.js
@@ -4,52 +4,60 @@ const costOptions = {
 	maxCost: 5000,
   };
   
-  const checkboxMoveSensor = document.getElementById("flexCheckDefault");
-  const checkboxDoorSensor = document.getElementById("flexCheckChecked");
+  let checkboxMoveSensor = document.querySelector(".flexCheckMove");
+  let checkboxAirSensor = document.querySelector(".flexCheckAir");
+  let checkboxTempSensor = document.querySelector(".flexCheckTemp");
+  let checkboxSmokeSensor = document.querySelector(".flexCheckSmoke");
+  
+  let moveSensor = checkboxMoveSensor.checked ? 500 : 0;
+  let airSensor = checkboxAirSensor.checked ? 780 : 0;
+  let tempSensor = checkboxTempSensor.checked ? 190 : 0;
+  let smokeSensor = checkboxSmokeSensor.checked ? 1110 : 0;
+  
   let totalCost = costOptions.medCost;
+  
   let totalCostElement = document.getElementById("total_cost");
   
-  const btnradio1 = document.getElementById("btnradio1");
-  const btnradio2 = document.getElementById("btnradio2");
-  const btnradio3 = document.getElementById("btnradio3");
+  checkboxMoveSensor.addEventListener("change", () => {
+	moveSensor = checkboxMoveSensor.checked ? 500 : 0;
+	updateTotalCost();
+  });
+  
+  checkboxAirSensor.addEventListener("change", () => {
+	airSensor = checkboxAirSensor.checked ? 780 : 0;
+	updateTotalCost();
+  });
+  
+  checkboxTempSensor.addEventListener("change", () => {
+	tempSensor = checkboxTempSensor.checked ? 190 : 0;
+	updateTotalCost();
+  });
+  
+  checkboxSmokeSensor.addEventListener("change", () => {
+	smokeSensor = checkboxSmokeSensor.checked ? 1110 : 0;
+	updateTotalCost();
+  });
+  
+  let btnradio1 = document.getElementById("btnradio1");
+  let btnradio2 = document.getElementById("btnradio2");
+  let btnradio3 = document.getElementById("btnradio3");
   
   btnradio1.addEventListener("click", () => {
-	totalCost = calculateTotalCost();
+	totalCost = costOptions.minCost;
 	updateTotalCost();
   });
   
   btnradio2.addEventListener("click", () => {
-	totalCost = calculateTotalCost();
+	totalCost = costOptions.medCost;
 	updateTotalCost();
   });
   
   btnradio3.addEventListener("click", () => {
-	totalCost = calculateTotalCost();
+	totalCost = costOptions.maxCost;
 	updateTotalCost();
   });
-  
-  checkboxMoveSensor.addEventListener("change", () => {
-	totalCost = calculateTotalCost();
-	updateTotalCost();
-  });
-  
-  checkboxDoorSensor.addEventListener("change", () => {
-	totalCost = calculateTotalCost();
-	updateTotalCost();
-  });
-  
-  function calculateTotalCost() {
-	let additionalCost = 0;
-	if (checkboxMoveSensor.checked) {
-	  additionalCost += 250;
-	}
-	if (checkboxDoorSensor.checked) {
-	  additionalCost += 110;
-	}
-	return totalCost + additionalCost;
-  }
   
   function updateTotalCost() {
-	totalCostElement.innerText = totalCost;
+	totalCostElement.innerText = totalCost + moveSensor + airSensor + tempSensor + smokeSensor;
   }
   
