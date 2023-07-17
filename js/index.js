6f4e7dfc82acd11497a6a519b88a318ff8f3e827
satedone <nz@intop.dp.ua>
2023-07-17 20:29:39 +0300

    add lose coma

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     Promise.all([
       import('./btnjs.js'),
-      import('./calc.js')
+      import('./calc.js'),
       import('./telegram.js')
     ]).then(([btnjsModule, calcModule]) => {
     });
